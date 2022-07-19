5974b8c4648e80e27b145521f74415b86cdf7484
torch96 <joetorch96@gmail.com>
2022-07-19 13:37:10 -0400

    route change

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -4,12 +4,12 @@ import ReviewsCtrl from "./reviews.controller.js"
 
 const router = express.Router()
 
-router.route("/").get(moviesCtrl.apiGetmovies)
+router.route("/movies").get(moviesCtrl.apiGetmovies)
 router.route("/id/:id").get(moviesCtrl.apiGetMovieById)
-router.route("/genres").get(moviesCtrl.apiGetMovieGenres)
+//router.route("/genres").get(moviesCtrl.apiGetMovieGenres)
 
 router
-  .route("/review")
+  .route("/comments")
   .post(ReviewsCtrl.apiPostReview)
   .put(ReviewsCtrl.apiUpdateReview)
   .delete(ReviewsCtrl.apiDeleteReview)
