fc3cdcf07444a6f8e9fcfff1407ee04e222d9716
torch96 <joetorch96@gmail.com>
2022-07-20 12:21:11 -0400

    movie page update

--- frontend/src/components/movies.js
+++ frontend/src/components/movies.js
@@ -7,11 +7,12 @@ const Movie = props => {
     id: null,
     title: "",
     poster: "",
-    plot: "",
+    fullplot: "",
     imdb: {},
     genres: {},
     cast:{},
-    text: []
+    num_mflix_comments: 0,
+    comments: []
   };
   const [movie, setMovie] = useState(initialMovieState);
 
@@ -20,6 +21,7 @@ const Movie = props => {
       .then(response => {
         setMovie(response.data);
         console.log(response.data);
+        console.log(id);
       })
       .catch(e => {
         console.log(e,id);
@@ -28,6 +30,7 @@ const Movie = props => {
 
   useEffect(() => {
     getMovie(props.match.params.id);
+    
   }, [props.match.params.id]);
 
   const deleteReview = (commentsId, index) => {
@@ -47,21 +50,58 @@ const Movie = props => {
 
   return (
     <div>
-      {movie ? (
+      {movie ? (   
         <div>
           <h5>{movie.title}</h5>
+          <img src={movie.poster} className="poster" ></img>
           <p>
-            <strong>Plot: </strong>{movie.plot}<br/>
+            <strong>Plot: </strong>{movie.fullplot}<br/>
             <strong>imdb: </strong>
           </p>
+
+
+            
           <Link to={"/movies/" + props.match.params.id + "/review"} className="btn btn-primary">
             Add Review
           </Link>
           <h4> Reviews </h4>
-          
-
-        </div>
-      ) : (
+          <div className="row">
+            {movie.num_mflix_comments
+            > 0 ? (
+             movie.comments.map((comment, index) => {
+               return (
+                 <div className="col-lg-4 pb-1" key={index}>
+                   <div className="card">
+                     <div className="card-body">
+                       <p className="card-text">
+                         {comment.text}<br/>
+                         <strong>User: </strong>{comment.name}<br/>
+                         <strong>Date: </strong>{comment.date}
+                       </p>
+                       {props.user && props.user.id === comment.user_id &&
+                          <div className="row">
+                            <a onClick={() => deleteReview(comment._id, index)} className="btn btn-primary col-lg-5 mx-1 mb-1">Delete</a>
+                            <Link to={{
+                              pathname: "/movies/" + props.match.params.id + "/review",
+                              state: {
+                                currentReview: comment
+                              }
+                            }} className="btn btn-primary col-lg-5 mx-1 mb-1">Edit</Link>
+                          </div>                   
+                       }
+                     </div>
+                   </div>
+                 </div>
+               );
+             })
+             ) : (
+              <div className="col-sm-4">
+                <p>No reviews yet.</p>
+              </div>
+              )}
+            </div>
+          </div>
+        ) : (
         <div>
           <br />
           <p>No movie selected.</p>
