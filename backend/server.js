165ca115b6acce997dc1195fbf4cd094e1bdaae8
torch96 <joetorch96@gmail.com>
2022-07-24 14:34:34 -0400

    user route added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express"
+import morgan from "morgan"
+import bodyParser from "body-parser"
 import cors from "cors"
 import mongoose from "mongoose"
 import movies from "./api/movies.route.js"
+import userRoute from "./api/user.route.js"
 
 const app = express()
 
@@ -9,6 +12,10 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/api/v1/",movies)
+app.use("/api/v1/user",userRoute)
+app.use("/status", express.static("build"))
+app.use("/", express.static("build"))
+
 app.use("*", (req,res) => res.status(404).json({error: "not found"}))
 
 export default app 
