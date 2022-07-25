839c37fe462f28cd8e895f62bff415c11dbfb7c7
torch96 <joetorch96@gmail.com>
2022-07-25 19:59:34 -0400

    movie route

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,13 +1,15 @@
 import express from "express"
 import moviesCtrl from "./movies.controller.js"
 import ReviewsCtrl from "./reviews.controller.js"
-import userCtrl from "./user.controller.js"
+
 
 const router = express.Router()
 
 router.route("/movies").get(moviesCtrl.apiGetmovies)
 router.route("/id/:id").get(moviesCtrl.apiGetMovieById)
-//router.route("/genres").get(moviesCtrl.apiGetMovieGenres)
+router.route("/genres").get(moviesCtrl.apiGetMovieGenres)
+
+
 
 router
   .route("/comments")
