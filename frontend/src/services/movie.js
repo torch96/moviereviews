0747b7490dd808975c1d4d4bd48e116046e564a7
torch96 <joetorch96@gmail.com>
2022-08-07 11:19:28 -0400

    route change

--- frontend/src/services/movie.js
+++ frontend/src/services/movie.js
@@ -1,12 +1,12 @@
 import http from "../http-common";
-
+const URL = "http://localhost:5000/api/v1";
 class MovieDataService {
   getAll(page = 0) {
     return http.get(`movies?page=${page}`);
   }
 
   get(id) {
-    console.log(id);
+    
     return http.get(`/id/${id}`);
   }
 
@@ -14,16 +14,53 @@ class MovieDataService {
     return http.get(`movies?${by}=${query}&page=${page}`);
   } 
 
-  createReview(data) {
-    return http.post("/comments-new", data);
+  async createReview(data) {
+    return await fetch(URL + '/comments', {
+      method: 'POST',
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${data.jwt}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        movie_id: data.movie_id,
+        name: data.name,
+        text: data.text,
+      }),
+    })
+    
   }
 
-  updateReview(data) {
-    return http.put("/comments-edit", data);
+  async updateReview(data) {
+    return await fetch(URL + '/comments', {
+      method: 'PUT',
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${data.jwt}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        review_id: data.review_id,
+        name: data.name,
+        text: data.text,
+      }),
+    })
+    
   }
 
-  deleteReview(id, userId) {
-    return http.delete(`/comments-delete?id=${id}`, {data:{user_id: userId}});
+  async deleteReview(id, jwt ) {
+    
+    return await fetch(URL + '/comments', {
+    method: 'DELETE',
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        comment_id : id,
+      }),
+    })
   }
 
  /* getGenres(id) {
