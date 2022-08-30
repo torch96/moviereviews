919db18e43fbccd048d3bdcfc1881301e1d83c81
Jospeh <47431708+torch96@users.noreply.github.com>
2022-08-29 23:13:12 -0400

    build

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,22 @@ import cors from "cors"
 import mongoose from "mongoose"
 import movies from "./api/movies.route.js"
 import userRoute from "./api/user.route.js"
+import path from "path"
+import {fileURLToPath} from 'url';
 
 const app = express()
-
+const filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(filename);
 app.use(cors())
 process.env.NODE_ENV !== "prod" && app.use(morgan("dev"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend')))
+
+ /*   app.use(express.static(path.join(__dirname, '../frontend')))
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-    })
-}
+    })*/
+
 app.use("/api/v1/",movies)
 app.use("/api/v1/users",userRoute)
 app.use("/status", express.static("build"))
